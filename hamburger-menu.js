a7b79f35948a28fff0f5740dd5127af9415a8de2
Karel Šťastný <karell29@icloud.com>
2023-08-22 08:34:38 +0200

    oprava bugu

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,9 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const crossIcon = document.querySelector(".cross-icon");
+const hamburgerIcon = document.querySelector(".hamburger-icon.icon");
+const crossIcon = document.querySelector(".cross-icon.icon");
 
 hamburger.addEventListener("click", () => {
+    console.log("click");
     hamburger.classList.toggle("hidden");
     navMenu.classList.toggle("nav-menu-active");
     hamburgerIcon.classList.toggle("hidden");
