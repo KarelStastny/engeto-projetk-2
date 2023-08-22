f34a373a1e6b6547bd69993f603ed6650d86b027
Karel Šťastný <karell29@icloud.com>
2023-08-22 08:18:52 +0200

    dodělání projektu engeto 2

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,8 +1,11 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const crossIcon = document.querySelector(".cross-icon");
 
-
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hidden");
+    navMenu.classList.toggle("nav-menu-active");
+    hamburgerIcon.classList.toggle("hidden");
+    crossIcon.classList.toggle("active");
+});
